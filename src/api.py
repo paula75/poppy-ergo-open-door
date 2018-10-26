18184686f51c470ed9fad7ff3fc97dcd54b692db
paula75 <paula.ortega@inria.cl>
2018-10-26 10:43:40 -0300

    Add function to perform a primitive

--- src/api.py
+++ src/api.py
@@ -7,28 +7,45 @@ PORT= '8080'
 POPPY_ADDRESS = IP_ADDRESS + ':' + PORT
 
 
-PRIMITIVE='/primitive'
+movement=['/primitive','/motor']
+
 
 start= '/start.json'
 stop= '/stop.json'
 
 
 def list_primitives():
-    r = requests.get(POPPY_ADDRESS+PRIMITIVE+'/list.json')
+    r = requests.get(POPPY_ADDRESS+movement[0]+'/list.json')
     if r.status_code == 200:
         available_primitives = r.json()
         return available_primitives["primitives"]
 
-
-def do_action(primitive,action):
-    r = requests.get(POPPY_ADDRESS+action)
-    return r.status_code
+def do_primitive(list_primitives,action,option):
+    if action in list_primitives:
+        payload = '/' + action
+        payload = POPPY_ADDRESS+movement[0]+payload
+        if option=='start':
+            payload = payload+start
+        else:
+            payload = payload+stop
+        r = requests.get(payload)
+        if r.status_code == 200:
+            print('Success!')
+            return 1
+        else:
+            print(r.status_code)
+            return 0
+    else:
+        print(action + ' is not available')
+    return 0
 
 def show_us_dance():
-    do_action(start_dance)
+    list_prims = list_primitives()
+    do_primitive(list_prims,'dance','start')
     time.sleep(10)
-    do_action(stop_dance)
+    do_primitive(list_prims,'dance','stop')
 
-# show_us_dance()
 list_prim = list_primitives()
-print(list_prim)
+do_primitive(list_prim, 'get_up_posture','start')
+do_primitive(list_prim, 'get_up_posture','stop')
+show_us_dance()
